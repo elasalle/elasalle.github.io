c8db8e1c9740b67649e9f253b4e46dd40b9d2f16
elasalle <etienne.lasalle@inria.fr>
2021-03-26 19:31:21 +0100

    modifications to add abstracts

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
-function myFunction(id) {
-  var x = document.getElementById(id);
+function switchDisplay(x) {
   if (x.style.display === 'none') {
     x.style.display = 'block';
   } else {
     x.style.display = 'none';
+  }
+}
+
+function switchDisplayByClassName(className) {
+  var x = document.getElementsByClassName(className);
+  for (i=0 ; i<x.length ; i++) {
+    switchDisplay(x.item(i))
   }
 }
